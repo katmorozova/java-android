20c101a7f49fb0a46e29cd9999e9a7840357e34a
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-08 15:04:02 +0200

    Creados archivos Math.java y MainMath.java con su logica

---
+++ OOP/src/MainMath.java
@@ -0,0 +1,10 @@
+public class MainMath {
+
+    public static void main(String[] args) {
+
+        Math math = new Math();
+        System.out.println(math.multiple(10, 50));
+        System.out.println(math.multiple(2,4));
+        System.out.println(math.multiple(3));
+    }
+}
---
+++ OOP/src/Math.java
@@ -0,0 +1,14 @@
+public class Math {
+
+    static int multiple(int a, int b){
+        return  a + b;
+    }
+
+    static double multiple(double a, double b){
+        return  a + b;
+    }
+
+    static int multiple(int a){
+        return a + a;
+    }
+}

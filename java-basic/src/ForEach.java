3f95786103a2bbba7e454d9e38516764d03c5225
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-08 11:21:28 +0200

    Creado archivo ForEach.java con su logica

--- java-basic/src/ForEach.java
+++ java-basic/src/ForEach.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class ForEach {
+
+    public static void main(String[] args) {
+
+        String[] names = new String[]{
+                "Elena",
+                "Alfonso",
+                "Gabriela",
+                "Martin"
+        };
+
+        for(String name : names) {
+            System.out.println(name);
+        }
+
+        int[] numbers = new int[100];
+
+        for(int i = 0; i < numbers.length; ++i) {
+            numbers[i] = i + 100;
+            System.out.println(numbers[i]);
+        }
+
+        for(int number : numbers) {
+            System.out.println(number);
+        }
+
+
+}
 }

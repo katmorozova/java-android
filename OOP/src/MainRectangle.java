cb00dbb5cf4d754eafe57f08fa3b7ee4449e7dfb
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-08 14:34:55 +0200

    Creados archivos Rectangle.java y MainRectangle.java con su logica

---
+++ OOP/src/MainRectangle.java
@@ -0,0 +1,10 @@
+public class MainRectangle {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setDimens(12,23);
+        System.out.println(rectangle.getArea());
+
+    }
+}
---
+++ OOP/src/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+
+    int length;
+    int width;
+
+    void setDimens(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    int getArea(){
+        return length * width;
+    }
+
+}

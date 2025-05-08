a67a2c9c14962a0baafe28cd2eb6b8737ba47835
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-08 13:39:21 +0200

    Creados archivos Box.java y MainBox.java con su logica. Creados archivos Person.java y MainPerson

---
+++ OOP/src/MainBox.java
@@ -0,0 +1,23 @@
+public class MainBox {
+
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        box1.height = 10;
+        box1.width = 10;
+        box1.length = 10;
+
+        Box box2 = new Box();
+        box2.height = 20;
+        box2.width = 20;
+        box2.length = 20;
+
+        Box box3 = box2;
+
+        double volume1 = box1.height * box1.length * box1.width;
+        double volume2 = box2.height * box2.length * box2.width;
+        System.out.println(volume1);
+        System.out.println(volume2);
+
+
+    }
+}
---
+++ OOP/src/MainPerson.java
@@ -0,0 +1,7 @@
+public class MainPerson {
+
+    public static void main(String[] args) {
+
+
+    }
+}

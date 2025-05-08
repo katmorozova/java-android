aae789adc8f9b24ccc2beddd25617b6f35afa007
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-08 14:21:40 +0200

    Creados archivos Test.java y MainTest.java con su logica

---
+++ OOP/src/MainTest.java
@@ -0,0 +1,7 @@
+public class MainTest {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.square(20));
+    }
+}
---
+++ OOP/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    int square10(){
+        return 10;
+    }
+
+    int square(int number){
+        return number * number;
+    }
+}

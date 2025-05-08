9fa2692f8a27a13c21b6da5ce54764e6e83761a7
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-08 22:58:44 +0200

    Añadidos cambios en los archivos MainPerson.java y Person.java

--- OOP/src/MainPerson.java
+++ OOP/src/MainPerson.java
@@ -2,22 +2,28 @@ public class MainPerson {
 
     public static void main(String[] args) {
 
-    Person dany = new Person();
+    Person dany = new Person("Dany", 34, 76);
+    /*
     dany.name = "Dany";
     dany.age = 34;
     dany.weight = 76;
+     */
 
-    Person olga = new Person();
+    Person olga = new Person("Olga", 64, 53);
+    /*
     olga.name = "Olga";
     olga.age = 64;
     olga.weight = 53;
+     */
 
-    Person xavi = new Person();
+    Person xavi = new Person("Xavi", 46, 84);
+    /*
     xavi.name = "Xavi";
     xavi.age = 46;
     xavi.weight = 84;
+     */
 
-    double sumAges = dany.age + olga.age + xavi.age;
+    double sumAges = dany.getAge() + olga.getAge() + xavi.getAge();
     double average = sumAges / 3;
 
         System.out.println(average);

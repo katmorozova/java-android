4f515e47c65b7d3f722c14dc5711fad452fa2391
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-10 22:00:25 +0200

    Creado archivo MainAnimal.java con una parte de logica

---
+++ OOP/src/interfaces/MainAnimal.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public class MainAnimal {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        Fish fish = new Fish();
+    }
+}

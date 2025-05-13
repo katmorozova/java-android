295058b874ce3b0f39b0209ecd2f5db77f0454a9
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-13 14:14:16 +0200

    Creados archivos Multithreading.java y Timer.java con su logica

---
+++ OOP/src/Multithreading.java
@@ -0,0 +1,45 @@
+public class Multithreading {
+    static boolean isFive = false;
+
+    public static void main(String[] args) {
+        //Timer timer = new Timer();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 10_000_000; i++){
+                    if (i == 5){
+                        isFive = true;
+                    }
+                    System.out.println(i);
+                    try{
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){
+
+                    }
+
+                }
+            }
+        });
+        thread.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 10_000_000; i++){
+                    if(isFive){
+                        break;
+                    }
+                    System.out.println(i);
+                    try{
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){
+
+                    }
+
+                }
+            }
+        });
+        thread2.start();
+
+    }
+}

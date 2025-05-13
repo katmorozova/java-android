c4861aaed5885d160333834975db3f4a5607d349
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-13 22:41:43 +0200

    Añadida la logica en MainActivity de Calculadora

--- android/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ android/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculadora;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText editTextNumber = findViewById(R.id.editTextNumber);
+        Button buttonAnswer = findViewById(R.id.buttonAnswer);
+        TextView textViewAnswerIncorrect = findViewById(R.id.textViewAnswerIncorrect);
+        TextView textViewAnswerCorrect = findViewById(R.id.textViewAnswerCorrect);
+        TextView textViewExample = findViewById(R.id.textViewExample);
+
+        textViewExample.setText("10 + 20 = ?");
+
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editTextNumber.getText().toString();
+                int number = Integer.parseInt(text);
+                if(number == 30){
+                    textViewAnswerCorrect.setVisibility(View.VISIBLE);
+                    textViewAnswerIncorrect.setVisibility(View.GONE);
+                }else{
+                    textViewAnswerCorrect.setVisibility(View.GONE);
+                    textViewAnswerIncorrect.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 }

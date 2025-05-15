e05063a001595bb8458db1b8485ec4c37f46363e
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-15 22:31:37 +0200

    Cambios en idea añadidos. Añadidos cambios en archivo MainActivity

--- android/ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ android/ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton buttonAddNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+    private void initViews(){
+        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
     }
 }

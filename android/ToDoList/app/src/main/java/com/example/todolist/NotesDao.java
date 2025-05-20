93e8b24bae94e5ba245e5db7833f934e5f39bd9f
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-20 14:06:23 +0200

    Creado interface NotesDao con su logica

---
+++ android/ToDoList/app/src/main/java/com/example/todolist/NotesDao.java
@@ -0,0 +1,20 @@
+package com.example.todolist;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.ArrayList;
+
+@Dao
+public interface NotesDao {
+
+    @Query("SELECT * FROM notes")
+    ArrayList<Note> getNotes();
+
+    @Insert
+    void add(Note note);
+
+    @Query("DELETE FROM notes WHERE id = :id")
+    void remove(int id);
+}

88cc8fbfdf458ec3f2a70fe2848d88d0007bdeda
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-21 13:45:27 +0200

    Cambios en NotesDao añadidos

--- android/ToDoList/app/src/main/java/com/example/todolist/NotesDao.java
+++ android/ToDoList/app/src/main/java/com/example/todolist/NotesDao.java
@@ -4,13 +4,14 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
-import java.util.ArrayList;
+
+import java.util.List;
 
 @Dao
 public interface NotesDao {
 
     @Query("SELECT * FROM notes")
-    ArrayList<Note> getNotes();
+    List<Note> getNotes();
 
     @Insert
     void add(Note note);

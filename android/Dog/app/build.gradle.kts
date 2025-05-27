51179ace5881f9c79c55382860b1591d8850e164
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-27 14:43:42 +0200

    Añadida dependencia de libreria Glide en gradle

--- android/Dog/app/build.gradle.kts
+++ android/Dog/app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
     implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
     implementation ("io.reactivex.rxjava3:rxjava:3.1.5")
-    
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)

d8bb760bba750cf0c0b9ae80269bc0d4b2fbdc4b
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-28 15:07:51 +0200

    Añadidas dependencias para librerias Retrofit, RxJava, Room y Glide en Gradle

--- android/Movies/app/build.gradle.kts
+++ android/Movies/app/build.gradle.kts
@@ -37,6 +37,18 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("io.reactivex.rxjava3:rxandroid:3.0.2")
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.5")
+    implementation ("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+    val room_version = "2.7.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

07e732e546d786118b010c776fd27fee53f206c9
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-29 14:45:32 +0200

    Creado clase Movie con su logica

---
+++ android/Movies/app/src/main/java/com/example/movies/Movie.java
@@ -0,0 +1,44 @@
+package com.example.movies;
+
+public class Movie {
+
+    private int id;
+    private String name;
+    private String description;
+    private int year;
+    private Poster poster;
+    private Rating rating;
+
+    public Movie(int id, String name, String description, int year, Poster poster, Rating rating){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.year = year;
+        this.poster = poster;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Poster getPoster() {
+        return poster;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+}

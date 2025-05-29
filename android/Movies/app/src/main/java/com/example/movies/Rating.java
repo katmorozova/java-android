ad61f2ba87daea93ed66a9df71a41818190a0fb3
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-29 14:41:43 +0200

    Creado clase Rating con su logica

---
+++ android/Movies/app/src/main/java/com/example/movies/Rating.java
@@ -0,0 +1,15 @@
+package com.example.movies;
+
+public class Rating {
+
+    private String kp;
+
+
+    public Rating(String kp){
+        this.kp = kp;
+    }
+
+    public String getKp() {
+        return kp;
+    }
+}

202488daf7bccdcbc1a2a718f3a0f3dde8ebdb0d
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-02 14:50:50 +0200

    Añadido anotacion Query para page en ApiService

--- android/Movies/app/src/main/java/com/example/movies/ApiService.java
+++ android/Movies/app/src/main/java/com/example/movies/ApiService.java
@@ -2,8 +2,9 @@ package com.example.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("movie?token=CA4EP9E-R4F4XE7-J97X81W-V05R4WH&rating.kp=7-10&votes.kp=1000-6666666&limit=5")
-    Single<MovieResponse> loadMovies(int page);
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 }

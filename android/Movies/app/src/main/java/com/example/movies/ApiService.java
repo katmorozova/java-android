c3c733b7fe396355b947262e7789757f3cae7d4b
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-02 14:49:11 +0200

    Quitado de end-point valor de page y utilizado como parametro para loadMovies() en ApiService

--- android/Movies/app/src/main/java/com/example/movies/ApiService.java
+++ android/Movies/app/src/main/java/com/example/movies/ApiService.java
@@ -4,6 +4,6 @@ import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
 
 public interface ApiService {
-    @GET("movie?token=CA4EP9E-R4F4XE7-J97X81W-V05R4WH&rating.kp=7-10&votes.kp=1000-6666666&page=2&limit=5")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?token=CA4EP9E-R4F4XE7-J97X81W-V05R4WH&rating.kp=7-10&votes.kp=1000-6666666&limit=5")
+    Single<MovieResponse> loadMovies(int page);
 }

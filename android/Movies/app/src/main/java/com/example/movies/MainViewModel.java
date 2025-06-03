de50f4486df27a0f9ecb46fa0c8c9e524e1fdd77
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-03 15:47:41 +0200

    Establecido valor para isLoading en MainViewModel(visible progressBar o no)

--- android/Movies/app/src/main/java/com/example/movies/MainViewModel.java
+++ android/Movies/app/src/main/java/com/example/movies/MainViewModel.java
@@ -13,6 +13,7 @@ import java.util.List;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -43,6 +44,18 @@ public class MainViewModel extends AndroidViewModel {
         Disposable disposable = ApiFactory.apiService.loadMovies(page)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Throwable {
+                        isLoading.setValue(true);   //mostrar ProgressBar
+                    }
+                })
+                .doAfterTerminate(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        isLoading.setValue(false);   //no mostrar ProgressBar
+                    }
+                })
                 .subscribe(new Consumer<MovieResponse>() {
                     @Override
                     public void accept(MovieResponse movieResponse) throws Throwable {

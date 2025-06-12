65edfb2ea506d55d78590d509515e1b625b9b251
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-12 16:20:15 +0200

    Creado clase Poster con su logica

---
+++ android/Movies/app/src/main/java/com/example/movies/Poster.java
@@ -0,0 +1,21 @@
+package com.example.movies;
+
+public class Poster {
+
+    String url;
+
+    public Poster(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "Poster{" +
+                "url='" + url + '\'' +
+                '}';
+    }
+}

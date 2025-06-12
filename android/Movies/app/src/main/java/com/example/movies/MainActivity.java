fe652376d4457f78d0b6addfa62a525180fcf4e0
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-12 15:01:12 +0200

    Cambios en proyecto

--- android/Movies/app/src/main/java/com/example/movies/MainActivity.java
+++ android/Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -1,34 +1,14 @@
 package com.example.movies;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.functions.Consumer;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MainViewModel viewModel;
-
-    private RecyclerView recyclerViewMovies;
-    private MoviesAdapter moviesAdapter;
-    private ProgressBar progressBarLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,60 +20,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        progressBarLoading = findViewById(R.id.progressBarLoading);
-        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
-        moviesAdapter = new MoviesAdapter();
-        recyclerViewMovies.setAdapter(moviesAdapter);//insertamos Adapter en recyclerView
-        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));//insertamos layoutManager en RecyclerView para mostrar peliculas en 2 filas(GreedLayoutManager)
-
-        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
-            @Override
-            public void onChanged(List<Movie> movies) {
-                //en caso positivo -> estableser las peliculas en pantalla
-                Log.d("MainActivity", movies.toString());
-                moviesAdapter.setMovies(movies);
-            }
-        });
-        //viewModel.loadMovies();
-        viewModel.getIsLoading().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean isLoading) {
-                if(isLoading){
-                    progressBarLoading.setVisibility(View.VISIBLE);
-                }else{
-                    progressBarLoading.setVisibility(View.GONE);
-                }
-            }
-        });
-        moviesAdapter.setOnReachEndListener(new MoviesAdapter.OnReachEndListener() {
-            @Override
-            public void onReachEnd() {
-                viewModel.loadMovies();
-            }
-        });
-        moviesAdapter.setOnMovieClickListener(new MoviesAdapter.OnMovieClickListener() {
-            @Override
-            public void onMovieClick(Movie movie) {
-                Intent intent = MovieDetailActivity.newIntent(MainActivity.this,movie);
-                startActivity(intent);
-            }
-        });
-/*
-        ApiFactory.apiService.loadMovies()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MovieResponse>() {
-                    @Override
-                    public void accept(MovieResponse movieResponse) throws Throwable {
-                        Log.d("MainActivity", movieResponse.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.d("MainActivity",throwable.toString());
-                    }
-                });
- */
     }
 }

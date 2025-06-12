310f0aa056a5bab83d0176272de225671cefeed9
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-13 00:20:27 +0200

    Añadido MainViewModel en MainActivity con cambios relacionados

--- android/Movies/app/src/main/java/com/example/movies/MainActivity.java
+++ android/Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -8,12 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +31,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
+        @Override
+        public void onChanged(List<Movie> movies) {
+//en caso de exito mostrar en pantalla
+            Log.d("MainActivity", movies.toString());
+        }
+    });
+    viewModel.loadMovies();
 
-
-        ApiFactory.apiService.loadMovies()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MovieResponse>() {
-                    @Override
-                    public void accept(MovieResponse movieResponse) throws Throwable {
-                        Log.d("MainActivity", movieResponse.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.d("MainActivity", throwable.toString());
-                    }
-                });
     }
 }

5347160d63ab72dd93e2a319862dbbce531ea9eb
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-16 14:35:55 +0200

    Cambios en metodo onBindViewHolder() añadidos en MovieAdapter(para mostrar rating en diferente color). Camios relacionados añadidos en Rating

--- android/Movies/app/src/main/java/com/example/movies/MoviesAdapter.java
+++ android/Movies/app/src/main/java/com/example/movies/MoviesAdapter.java
@@ -1,5 +1,6 @@
 package com.example.movies;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -41,7 +43,23 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
         Glide.with(holder.itemView)
                 .load(movie.getPoster().getUrl())
                 .into(holder.imageViewPoster);
-        holder.textViewRating.setText(movie.getRating().getKp());
+
+        //obtenemos rating
+        double rating = movie.getRating().getKp();
+        //obtenemos id drawable archivo
+        int backgroundId;
+        if(rating > 7){
+            backgroundId = R.drawable.circle_green;
+        }else if(rating > 5){
+            backgroundId = R.drawable.circle_orange;
+        }else{
+            backgroundId = R.drawable.circle_red;
+        }
+        //obtenemos background
+        Drawable background = ContextCompat.getDrawable(holder.itemView.getContext(), backgroundId);
+        //insertamos background en textViewRating
+        holder.textViewRating.setBackground(background);
+        holder.textViewRating.setText(String.valueOf(rating));
     }
 
     @Override

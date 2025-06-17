b366fb1ba45ad0df4b56ab5f478b605ab197161d
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-17 15:32:32 +0200

    Creado MovieDetailViewModel con su constructor

---
+++ android/Movies/app/src/main/java/com/example/movies/MovieDetailViewModel.java
@@ -0,0 +1,14 @@
+package com.example.movies;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MovieDetailViewModel extends AndroidViewModel {
+
+
+    public MovieDetailViewModel(@NonNull Application application) {
+        super(application);
+    }
+}

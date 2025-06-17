eb1b7590f739a0fdeee29690eb6bdbd4c7d51b83
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-17 15:20:33 +0200

    Añadido metodo para cargar trailers en ApiService

--- android/Movies/app/src/main/java/com/example/movies/ApiService.java
+++ android/Movies/app/src/main/java/com/example/movies/ApiService.java
@@ -2,6 +2,7 @@ package com.example.movies;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 
@@ -11,4 +12,6 @@ public interface ApiService {
     //@GET("movie?token=CA4EP9E-R4F4XE7-J97X81W-V05R4WH&rating.kp=4-8&sortField=votes.kp&sortType=-1&notNullFields=videos.trailers.url&notNullFields=poster.url&limit=100")
     Single<MovieResponse> loadMovies(@Query("page")int page);
 
+    @GET("movie/{id}?token=CA4EP9E-R4F4XE7-J97X81W-V05R4WH")
+    Single<TrailerResponse> loadTrailers(@Path("id") int id);
 }

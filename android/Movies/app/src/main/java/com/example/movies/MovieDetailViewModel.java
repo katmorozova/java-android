1296a4d8de45dbffb22cd4bab08b64d698d2afa0
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-17 21:40:00 +0200

    Creado collecion de objeto LiveData con su getter en MovieDetailViewModel

--- android/Movies/app/src/main/java/com/example/movies/MovieDetailViewModel.java
+++ android/Movies/app/src/main/java/com/example/movies/MovieDetailViewModel.java
@@ -4,11 +4,20 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
 
 public class MovieDetailViewModel extends AndroidViewModel {
 
+    private MutableLiveData<List<Trailer>> trailers = new MutableLiveData<>();
 
     public MovieDetailViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public LiveData<List<Trailer>> getTrailers() {
+        return trailers;
     }
 }

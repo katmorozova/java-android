df118da169aad18d7af2bb31ed94d3376cc856f7
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-17 21:45:01 +0200

    Creado metodo loadTrailers(),añadido CompositeDisposable y metodo onCleared() en MovieDetailViewModel

--- android/Movies/app/src/main/java/com/example/movies/MovieDetailViewModel.java
+++ android/Movies/app/src/main/java/com/example/movies/MovieDetailViewModel.java
@@ -1,6 +1,7 @@
 package com.example.movies;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -9,8 +10,15 @@ import androidx.lifecycle.MutableLiveData;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class MovieDetailViewModel extends AndroidViewModel {
 
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     private MutableLiveData<List<Trailer>> trailers = new MutableLiveData<>();
 
     public MovieDetailViewModel(@NonNull Application application) {
@@ -19,5 +27,30 @@ public class MovieDetailViewModel extends AndroidViewModel {
 
     public LiveData<List<Trailer>> getTrailers() {
         return trailers;
+    }
+
+    public void loadTrailers(int id){
+        //enviamos peticion para cargar los trailers
+        Disposable disposable = ApiFactory.apiService.loadTrailers(id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<TrailerResponse>() {
+                    @Override
+                    public void accept(TrailerResponse trailerResponse) throws Throwable {
+                        Log.d("MovieDetailViewModel", trailerResponse.toString());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Throwable {
+                        Log.d("MovieDetailViewModel",throwable.toString() );
+                    }
+                });
+        compositeDisposable.add(disposable);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }

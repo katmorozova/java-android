e91ce014250b27e9d8594f35c36d49991711216b
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-27 22:27:58 +0200

    Creados activity_favourite_movie.xml y FavouriteMovieActivity con su logica

---
+++ android/Movies/app/src/main/java/com/example/movies/FavouriteMovieActivity.java
@@ -0,0 +1,35 @@
+package com.example.movies;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FavouriteMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_favourite_movie);
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+         */
+
+        RecyclerView recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        MoviesAdapter moviesAdapter = new MoviesAdapter();
+        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerViewMovies.setAdapter(moviesAdapter);
+
+    }
+}

67e783b1d20d7343f4c416f1fb12914acea55462
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-27 22:59:37 +0200

    Creado main_menu.xml con su logica. Cambios relacionados para mostrar menu añadidos en MainActivity

--- android/Movies/app/src/main/java/com/example/movies/MainActivity.java
+++ android/Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.movies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -40,9 +43,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+/*
         Intent intent = FavouriteMovieActivity.newIntent(this);
         startActivity(intent);
+
+ */
 
         progressBarLoading = findViewById(R.id.progressBarLoading);
         recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
@@ -90,5 +95,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //obtenemos ejemplar de clase MenuInflator
+        getMenuInflater().inflate(R.menu.main_menu, menu);//se muestra menu en la pantalla
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.itemFavourite){
+            Intent intent = FavouriteMovieActivity.newIntent(this);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

ff471ff24a39f4ef09244571e202bcace964388d
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-27 15:52:57 +0200

    Creado archivo MovieDatabase con su logica. Cambios relacionados añadidos en Movie

--- android/Movies/app/src/main/java/com/example/movies/Movie.java
+++ android/Movies/app/src/main/java/com/example/movies/Movie.java
@@ -1,11 +1,16 @@
 package com.example.movies;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 
+@Entity(tableName = "favourite_movies")
 public class Movie implements Serializable {
 
+    @PrimaryKey
     @SerializedName("id")
     private int id;
     @SerializedName("name")

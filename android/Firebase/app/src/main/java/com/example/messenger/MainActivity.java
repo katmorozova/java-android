d6f56ee7197001efa35aa41470b8708386170cb9
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-30 15:20:21 +0200

    Agregado FireBaseAuth en MainActivity

--- android/Firebase/app/src/main/java/com/example/messenger/MainActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/MainActivity.java
@@ -8,7 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        auth = FirebaseAuth.getInstance();
     }
 }

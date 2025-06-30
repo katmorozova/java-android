29fe5c09aaf3860691674e34049cd1c6a313e1d2
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-06-30 15:53:32 +0200

    Cambios en dependencias añadidas. Registramos nuevo usuario en MainActivity

--- android/Firebase/app/src/main/java/com/example/messenger/MainActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.messenger;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -34,5 +39,24 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Log.d("MainActivity", "Autorized");
         }
+        //registramos nuevo usuario
+        auth.createUserWithEmailAndPassword("email@email.com", "123456").addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                //obtenemos usuario
+                FirebaseUser user = auth.getCurrentUser();
+                if (user == null){
+                    Log.d("MainActivity", "Not autorized");
+                }else{
+                    Log.d("MainActivity", "Autorized");
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("MainActivity", e.getMessage());
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

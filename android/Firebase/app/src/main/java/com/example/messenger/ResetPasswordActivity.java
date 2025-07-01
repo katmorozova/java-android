20eb86bace785ab6175461f9f4ec7289f1285e25
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-01 23:28:49 +0200

    Asignadas variables del diseño y su valor en ResetPasswordActivity

--- android/Firebase/app/src/main/java/com/example/messenger/ResetPasswordActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/ResetPasswordActivity.java
@@ -1,6 +1,8 @@
 package com.example.messenger;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private Button buttonReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,11 @@ public class ResetPasswordActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+    private void initViews(){
+        editTextEmail = findViewById(R.id.editTextEmail);
+        buttonReset = findViewById(R.id.buttonReset);
     }
 }

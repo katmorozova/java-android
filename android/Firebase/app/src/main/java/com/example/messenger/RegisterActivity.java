7e8637d3704dda745c9299b739fe41b2226d1015
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-01 23:22:52 +0200

    Asignadas variables y su valor en RegisterActivity del diseño

--- android/Firebase/app/src/main/java/com/example/messenger/RegisterActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.messenger;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextName;
+    private EditText editTextLastName;
+    private EditText editTextAge;
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,15 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+    }
+
+    private void initViews(){
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAge = findViewById(R.id.editTextAge);
+        buttonRegister = findViewById(R.id.buttonRegister);
     }
 }

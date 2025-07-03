5f6be541b7e089cfda216c7937b9d736e7acb806
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-03 23:40:33 +0200

    Creado RegisterViewModel con su logica

---
+++ android/Firebase/app/src/main/java/com/example/messenger/RegisterViewModel.java
@@ -0,0 +1,68 @@
+package com.example.messenger;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterViewModel extends ViewModel {
+
+    private FirebaseAuth auth;
+
+    //liveData para guardar mensaje de error
+    private MutableLiveData<String> error = new MutableLiveData<>();
+
+    //livedata para mostrar si el usuario esta autorizado o no
+    private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
+
+
+    public RegisterViewModel() {
+        auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+                    user.setValue(firebaseAuth.getCurrentUser());
+                }
+            }
+        });
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
+    }
+
+    public void signUp(
+            String email,
+            String password,
+            String name,
+            String lastName,
+            int age
+
+    ){
+        auth.createUserWithEmailAndPassword(email, password).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                error.setValue(e.getMessage());
+            }
+        });
+    }
+
+
+
+
+
+
+
+}

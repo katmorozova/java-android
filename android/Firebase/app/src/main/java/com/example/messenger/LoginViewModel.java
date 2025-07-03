451f60ab0dd63cce8a953dee8215f514c82b12da
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-03 22:28:06 +0200

    Creado LoginViewModel con su logica

---
+++ android/Firebase/app/src/main/java/com/example/messenger/LoginViewModel.java
@@ -0,0 +1,55 @@
+package com.example.messenger;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginViewModel extends ViewModel {
+
+    private FirebaseAuth auth;
+
+    //liveData para guardar mensaje de error
+    private MutableLiveData<String> error = new MutableLiveData<>();
+
+    //livedata para mostrar si el usuario esta autorizado o no
+    private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
+
+
+
+
+    public LoginViewModel() {
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<FirebaseUser> getUser() {
+        return user;
+    }
+
+    public void login(String email, String password){
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                        user.setValue(authResult.getUser());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        error.setValue(e.getMessage());
+                    }
+                });
+    }
+
+
+}

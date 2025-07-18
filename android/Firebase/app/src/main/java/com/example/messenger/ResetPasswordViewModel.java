03878fda2e417f364c775b4786177349aa615bad
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-18 07:49:15 +0200

    Creado ResetPasswordViewModel con su logica

---
+++ android/Firebase/app/src/main/java/com/example/messenger/ResetPasswordViewModel.java
@@ -0,0 +1,43 @@
+package com.example.messenger;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordViewModel extends ViewModel {
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private MutableLiveData<String> error = new MutableLiveData<>();
+    private MutableLiveData<Boolean> success = new MutableLiveData<>();
+
+    public LiveData<String> getError() {
+        return error;
+    }
+
+    public LiveData<Boolean> getSuccess() {
+        return success;
+    }
+
+    public void resetPassword(String email){
+        auth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+//cerrar la pantalla
+                success.setValue(true);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+//mostrar error
+                error.setValue(e.getMessage());
+            }
+        });
+    }
+
+
+}

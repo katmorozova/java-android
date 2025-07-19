5aeb9933f90daa7896d3e197ea20637e5ae89d2b
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-19 15:00:25 +0200

    Añadido Toolbar en activity_user.xml,UserActivity

--- android/Firebase/app/src/main/java/com/example/messenger/UserActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/UserActivity.java
@@ -27,7 +27,7 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recyclerViewUsers), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;

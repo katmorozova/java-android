dd578b4cf2babaf972086be06c49310ae11601f6
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-19 15:21:01 +0200

    Cambios en dependencias de firebase añadidos. Añadida dependencia para Database Realtime en gradle

--- android/Firebase/app/build.gradle.kts
+++ android/Firebase/app/build.gradle.kts
@@ -43,8 +43,9 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
 
-    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)

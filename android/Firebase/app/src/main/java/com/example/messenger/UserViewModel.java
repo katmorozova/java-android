30a97cd55b0a64ac72817c7de3df1b8b5fd37a0c
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-22 06:49:48 +0200

    Asignado variable y su valor en constructorpara mostrar lista de usuarios para DatabaseReference en UserViewModel

--- android/Firebase/app/src/main/java/com/example/messenger/UserViewModel.java
+++ android/Firebase/app/src/main/java/com/example/messenger/UserViewModel.java
@@ -7,12 +7,14 @@ import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class UserViewModel extends ViewModel {
 
     private FirebaseAuth auth;
     private FirebaseDatabase database;
+    private DatabaseReference userReference;
 
     //livedata para mostrar si el usuario esta autorizado o no
     private MutableLiveData<FirebaseUser> user = new MutableLiveData<>();
@@ -27,7 +29,8 @@ public class UserViewModel extends ViewModel {
                 //}
             }
         });
-        database  = FirebaseDatabase.getInstance();
+        database = FirebaseDatabase.getInstance();
+        userReference = database.getReference("Users");
     }
 
 

048579e917b309b7a78eaedf0af8b3f2107b86b6
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-22 23:18:08 +0200

    Cambios en ChatActivity añadidos( Asignado MessagesAdapter en ChatActivity con su logica)

--- android/Firebase/app/src/main/java/com/example/messenger/ChatActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/ChatActivity.java
@@ -1,5 +1,7 @@
 package com.example.messenger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,12 +14,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_CURRENT_USER_ID = "current_id";
+    private static final String EXTRA_OTHER_USER_ID = "other_id";
 
     private TextView textViewTitle;
     private View onlineStatus;
     private RecyclerView recyclerViewMessage;
     private ImageView imageViewSendMessage;
+
+    private MessagesAdapter messagesAdapter;
+
+    private String currentUserId;
+    private String otherUserId;
 
 
     @Override
@@ -31,6 +44,29 @@ public class ChatActivity extends AppCompatActivity {
             return insets;
         });
         initViews();
+        currentUserId = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
+        otherUserId = getIntent().getStringExtra(EXTRA_OTHER_USER_ID);
+        messagesAdapter = new MessagesAdapter(currentUserId);
+        recyclerViewMessage.setAdapter(messagesAdapter);
+        List<Message> messages = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            Message message = new Message(
+                    "Text"+i,
+                    currentUserId,
+                    otherUserId
+            );
+            messages.add(message);
+        }
+        for (int i = 0; i < 10; i++){
+            Message message = new Message(
+                    "Text"+i,
+                    otherUserId,
+                    currentUserId
+            );
+            messages.add(message);
+        }
+        messagesAdapter.setMessages(messages);
+
     }
 
 
@@ -40,6 +76,13 @@ public class ChatActivity extends AppCompatActivity {
         onlineStatus = findViewById(R.id.onlineStatus);
         recyclerViewMessage = findViewById(R.id.recyclerViewMessages);
         imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+
+    public static Intent newIntent(Context context, String currentUserId, String otherUserId){
+        Intent intent = new Intent(context, ChatActivity.class);
+        intent.putExtra(EXTRA_CURRENT_USER_ID, currentUserId);
+        intent.putExtra(EXTRA_OTHER_USER_ID, otherUserId);
+        return intent;
     }
 
 }

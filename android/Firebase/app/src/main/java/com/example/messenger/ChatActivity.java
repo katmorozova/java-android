605b4249903731c3342f5bc36e4aca974db8c5ab
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-22 22:20:55 +0200

    Asignadas variables y su valor para diseño en ChatActivity

--- android/Firebase/app/src/main/java/com/example/messenger/ChatActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/ChatActivity.java
@@ -1,14 +1,24 @@
 package com.example.messenger;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private View onlineStatus;
+    private RecyclerView recyclerViewMessage;
+    private ImageView imageViewSendMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,16 @@ public class ChatActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
     }
+
+
+
+    private void initViews(){
+        textViewTitle = findViewById(R.id.textViewTitle);
+        onlineStatus = findViewById(R.id.onlineStatus);
+        recyclerViewMessage = findViewById(R.id.recyclerViewMessages);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+
 }

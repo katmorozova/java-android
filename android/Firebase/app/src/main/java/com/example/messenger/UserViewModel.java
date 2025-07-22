100d9abc0a8d52a3134d1563a177872986234dc5
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-22 06:56:19 +0200

    Añadido evento ValueEventListener() para añadir usuarios en la lista en UserViewModel

--- android/Firebase/app/src/main/java/com/example/messenger/UserViewModel.java
+++ android/Firebase/app/src/main/java/com/example/messenger/UserViewModel.java
@@ -7,8 +7,14 @@ import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserViewModel extends ViewModel {
 
@@ -31,6 +37,21 @@ public class UserViewModel extends ViewModel {
         });
         database = FirebaseDatabase.getInstance();
         userReference = database.getReference("Users");
+        userReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    List<User> usersFromDb = new ArrayList<>();
+                    User user = dataSnapshot.getValue(User.class);
+                    usersFromDb.add(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 

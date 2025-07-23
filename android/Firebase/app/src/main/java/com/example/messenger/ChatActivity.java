54c6d41e50959c1aea28c83fb3739949cf805bfb
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-07-23 14:46:58 +0200

    Inscribimos a liveData en metodo observeViewModel en ChatActivity

--- android/Firebase/app/src/main/java/com/example/messenger/ChatActivity.java
+++ android/Firebase/app/src/main/java/com/example/messenger/ChatActivity.java
@@ -4,14 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,6 +30,7 @@ public class ChatActivity extends AppCompatActivity {
     private View onlineStatus;
     private RecyclerView recyclerViewMessage;
     private ImageView imageViewSendMessage;
+    private EditText editTextMessage;
 
     private MessagesAdapter messagesAdapter;
 
@@ -55,32 +59,49 @@ public class ChatActivity extends AppCompatActivity {
 
         messagesAdapter = new MessagesAdapter(currentUserId);
         recyclerViewMessage.setAdapter(messagesAdapter);
-        List<Message> messages = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
-            Message message = new Message(
-                    "Text"+i,
-                    currentUserId,
-                    otherUserId
-            );
-            messages.add(message);
-        }
-        for (int i = 0; i < 10; i++){
-            Message message = new Message(
-                    "Text"+i,
-                    otherUserId,
-                    currentUserId
-            );
-            messages.add(message);
-        }
-        messagesAdapter.setMessages(messages);
+
+        observeViewModel();
+
 
     }
 
-
+    private void observeViewModel(){
+        viewModel.getMessages().observe(this, new Observer<List<Message>>() {
+            @Override
+            public void onChanged(List<Message> messages) {
+                messagesAdapter.setMessages(messages);
+            }
+        });
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if(errorMessage != null){
+                    Toast.makeText(ChatActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        viewModel.getMessageSent().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean sent) {
+                if(sent){
+                    editTextMessage.setText("");
+                }
+            }
+        });
+        viewModel.getOtherUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                //insertat texto en textViewTitle
+                String userInfo = String.format("%s %s", user.getName(), user.getLastName());
+                textViewTitle.setText(userInfo);
+            }
+        });
+    }
 
     private void initViews(){
         textViewTitle = findViewById(R.id.textViewTitle);
         onlineStatus = findViewById(R.id.onlineStatus);
+        editTextMessage = findViewById(R.id.editTextMessage);
         recyclerViewMessage = findViewById(R.id.recyclerViewMessages);
         imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
     }
